f53f21c2e3e56f4e02dc049c90a19a145ab7ee54
Chanchal Soni <chanchalsoni1998@gmail.com>
2022-12-17 21:03:34 +0530

    projects

--- script.js
+++ script.js
@@ -71,3 +71,9 @@ $(document).ready(function () {
     },
   });
 });
+document.getElementById("pr1").addEventListener("click", ()=> window.open('https://wondrous-marigold-ae1bc8.netlify.app/','_blank') );
+document.getElementById("pr11").addEventListener("click", ()=> window.open('https://github.com/chanchal-soni98/direful-order','_blank') );
+document.getElementById("pr2").addEventListener("click", ()=> window.open('https://capable-dragon-d91ac8.netlify.app/','_blank') );
+document.getElementById("pr21").addEventListener("click", ()=> window.open('https://github.com/chanchal-soni98/outgoing-mine-4990','_blank') );
+
+
